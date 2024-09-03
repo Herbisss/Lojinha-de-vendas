cb908b03743e4897f6941119431561d9fede8689
Herbisss <00001088689012sp@aluno.educacao.sp.gov.br>
2024-09-03 07:57:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let carrinho = [];
+let total = 0;
 
+function adicionarAoCarrinho(produto, preco) {
+    carrinho.push({ produto, preco });
+    total += preco;
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+    const carrinhoLista = document.getElementById('carrinho');
+    carrinhoLista.innerHTML = '';
+    
+    carrinho.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = ${item.produto} - R$ ${item.preco};
+        carrinhoLista.appendChild(li);
+    });
+    
+    document.getElementById('total').textContent = Total: R$ ${total};
+}
